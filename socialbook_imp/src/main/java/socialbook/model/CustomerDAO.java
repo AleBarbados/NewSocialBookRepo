73b1f6413c3110e44bfef6a5e63fa2a14b7dc740
Pandin00 <luketto.222000@gmail.com>
2021-01-14 10:40:23 +0100

    Voglio lievemente estinguermi

--- socialbook_imp/src/main/java/socialbook/model/CustomerDAO.java
+++ socialbook_imp/src/main/java/socialbook/model/CustomerDAO.java
@@ -1,4 +1,24 @@
 package socialbook.model;
-
+import java.sql.*;
 public class CustomerDAO {
+    public Customer doRetriveById(int id){
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT id_customer, customer_name, customer_surname, customer_pwd, customer_usr FROM customer WHERE id=?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Customer c = new Customer();
+                c.setId_customer(rs.getInt(1));
+                c.setC_name(rs.getString(2));
+                c.setC_surname(rs.getString(3));
+                c.setC_pwd(rs.getString(4));
+                c.setC_usr(rs.getString(5));
+                return c;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
