751052d1879092a3f3eec68d2e49315bc8fcb226
Pandin00 <luketto.222000@gmail.com>
2021-01-19 12:52:29 +0100

    Merge branch 'develop' of C:\Users\luket\Desktop\Socialbook with conflicts.

---
+++ socialbook_imp/src/main/java/socialbook/controller/FollowersServlet.java
@@ -0,0 +1,35 @@
+package socialbook.controller;
+
+import socialbook.model.Follow;
+import socialbook.model.FollowDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/FollowersServlet")
+public class FollowersServlet extends HttpServlet {
+    private static FollowDAO followDAO = new FollowDAO();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<Follow> follows = new ArrayList<>();
+        if(request.getParameter("Following")!=null){
+            follows=followDAO.doRetriveAllFollowed(Integer.parseInt(request.getParameter("id")));
+            request.setAttribute("Following", "Following");
+        }else if(request.getParameter("Followers")!=null){
+            follows=followDAO.doRetriveAllFollowers(Integer.parseInt(request.getParameter("id")));
+            request.setAttribute("Followers", "Followers");
+        }
+        request.setAttribute("follows", follows);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/followers.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
---
+++ socialbook_imp/src/main/webapp/WEB-INF/jsp/followers.jsp
@@ -0,0 +1,47 @@
+<%@ page import="socialbook.model.CustomerDAO" %>
+<%@ page import="socialbook.model.Customer" %>
+<%@ page import="socialbook.model.Follow" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: luket
+  Date: 19/01/2021
+  Time: 11:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>followers</title>
+</head>
+<body>
+<%
+    CustomerDAO customerDAO = new CustomerDAO();
+    ArrayList<Follow> follows = (ArrayList<Follow>) request.getAttribute("follows");
+%>
+<table>
+    <%if(request.getAttribute("Following")!=null){
+        if(follows.isEmpty()){%>
+            <h1>Non segue nessuno</h1>
+        <%}
+        for (Follow follow:
+            follows ) {
+            Customer user = customerDAO.doRetriveById(follow.getCostumer());
+        %>
+        <tr><td><%=user.getC_usr()%></td><td><%=user.getDescription()%>></td></tr>
+<%}%>
+    <%}else if(request.getAttribute("Followers")!=null){
+        if(follows.isEmpty()){%>
+    <h1>Non ha seguaci</h1>
+    <%}
+    for (Follow follow:
+            follows ) {
+        Customer user = customerDAO.doRetriveById(follow.getFollower());
+    %>
+    <tr><td><%=user.getC_usr()%></td><td><%=user.getDescription()%>></td></tr>
+    <%}%>
+    <%}%>
+</table>
+</body>
+</html>
