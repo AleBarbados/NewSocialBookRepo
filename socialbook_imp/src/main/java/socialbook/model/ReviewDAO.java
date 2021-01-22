355128c7e869f848748b540d5b3d4e271cdf2b9e
sasa71 <salva.russo10@gmail.com>
2021-01-22 12:07:47 +0100

    AggiunteDAO

--- socialbook_imp/src/main/java/socialbook/model/ReviewDAO.java
+++ socialbook_imp/src/main/java/socialbook/model/ReviewDAO.java
@@ -4,14 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ReviewDAO {
     private final static String DO_SAVE = "INSERT INTO review (id_review, id_customer, ISBN, review_date, body, vote) VALUES (?,?,?,?,?,?)";
     private final static String DO_DELETE_BY_ID = "DELETE FROM review WHERE id_review = ?";
     private final static String DO_RETRIEVE_BY_ID = "SELECT id_review, id_customer, ISBN, review_date, body, vote FROM review WHERE id_review = ?";
+    private final static String DO_RETRIEVE_BY_ISBN = "SELECT id_review, id_customer, ISBN, review_date, body, vote FROM review WHERE ISBN = ?";
 
     public void doSave(Review r) {
-        try(Connection con = ConPool.getConnection()) {
+        try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement(DO_SAVE);
 
             ps.setInt(1, r.getId_review());
@@ -24,35 +26,35 @@ public class ReviewDAO {
             if (ps.executeUpdate() != 1) {
                 throw new RuntimeException("INSERT error.");
             }
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void doDeleteById(int id) {
-        try(Connection con = ConPool.getConnection()) {
+        try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement(DO_DELETE_BY_ID);
             ps.setInt(1, id);
 
             if (ps.executeUpdate() != 1) {
                 throw new RuntimeException("DELETE error.");
             }
-        }catch(SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public Review doRetrieveById(int id_review) {
-        try(Connection con = ConPool.getConnection()) {
+        try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement(DO_RETRIEVE_BY_ID);
             ps.setInt(1, id_review);
 
             ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
+            if (rs.next()) {
                 return createReview(rs);
             }
             return null;
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
@@ -68,5 +70,21 @@ public class ReviewDAO {
         r.setVote(rs.getInt(6));
 
         return r;
+    }
+
+    public ArrayList<Review> doRetrieveByISBN(String isbn) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(DO_RETRIEVE_BY_ISBN);
+
+            ArrayList<Review> reviews = new ArrayList<>();
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                reviews.add(createReview(rs));
+            }
+            return reviews;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
